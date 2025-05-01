4caa7e3e2e6ee8d7123da93cf58a4285a3ba7f77
jerbo2 <bjeremyc@live.com>
2025-04-30 21:47:09 -0400

    edit config related things for deployment

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,9 +1,13 @@
+import sys
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -13,6 +17,8 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+config.set_main_option('sqlalchemy.url', os.getenv("DATABASE_URL"))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
